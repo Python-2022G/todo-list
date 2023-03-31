1bd151817c5dbc945f4299c02ea4f9d133f8e597
JavohirJalilov <javohirjaliloff@gmail.com>
2023-03-31 11:18:47 +0500

    all url

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import get_all
-
+from .views import Tasks, Delete, Update, CompletedTasks, InCompletedTasks
 
 urlpatterns = [
-    path('tasks', get_all)
+    path('tasks', Tasks.as_view()),
+    path('delete/<int:pk>', Delete.as_view()),
+    path('update/<int:pk>', Update.as_view()),
+    path('completed', CompletedTasks.as_view()),
+    path('incompleted', InCompletedTasks.as_view())
 ]
