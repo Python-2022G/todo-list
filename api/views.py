56ab054c7fef48a775e7a21398ab6a156b20bb03
JavohirJalilov <javohirjaliloff@gmail.com>
2023-03-31 11:18:33 +0500

    complate, incomplate, tasks, delete, update class view

--- api/views.py
+++ api/views.py
@@ -1,19 +1,113 @@
 from django.http import HttpRequest, JsonResponse
 from .models import Task
+import json
+from django.core.exceptions import ObjectDoesNotExist
+from django.views import View
 
-def get_all(request: HttpRequest) -> JsonResponse:
-    '''get all tasks'''
-    tasks = Task.objects.all()
-    data = {'tasks': []}
+def to_dict(task):
+    data = {
+        'id': task.id,
+        'name': task.name,
+        'completed': task.completed,
+        'desciption': task.description,
+        'created': task.created,
+        'updated': task.updated,
+    }
+    return data
 
-    for task in tasks:
-        data['tasks'].append({
-            'id': task.id,
-            'name': task.name,
-            'completed': task.completed,
-            'desciption': task.description,
-            'created': task.created,
-            'updated': task.updated,
-        })
+class Tasks(View):
+    def get(self, request: HttpRequest) -> JsonResponse:
+        '''get all tasks'''
+        tasks = Task.objects.all()
+        data = {'tasks': []}
 
-    return JsonResponse(data)
+        for task in tasks:
+            data['tasks'].append(to_dict(task))
+
+        return JsonResponse(data)
+    
+    def post(self, request: HttpRequest) -> JsonResponse:
+        """
+        Creta todo task
+        """
+        if request.method == 'POST':
+            body = request.body
+            # decode body
+            decode = body.decode()
+            data = json.loads(decode)
+            name = data.get('name')
+            completed = data.get('completed')
+            description = data.get('description')
+
+            if name == None:
+                return JsonResponse({"status": "name field is required."})
+            if description == None:
+                return JsonResponse({"status": "description field is required."})
+            
+            task = Task.objects.create(
+                name = name,
+                completed = completed,
+                description = description
+            )
+            task.save()
+            return JsonResponse({"status": "Saccessfuly!"})
+        else:
+            return JsonResponse({"status": "You need POST request!"})
+    
+class Delete(View):
+    def get(self, request: HttpRequest, pk: int) -> JsonResponse:
+        """
+        delete task
+        """
+        if request.method == "GET":
+            try:
+                # get product from database by id
+                product = Task.objects.get(id=pk)
+                product.delete()
+                return JsonResponse(to_dict(product))
+            except ObjectDoesNotExist:
+                return JsonResponse({"status": "object doesn't exist"})
+   
+class Update(View):
+    def post(self, request: HttpRequest, pk: int) -> JsonResponse:
+        if request.method == "POST":
+            try:
+                task = Task.objects.get(id=pk)
+            except:
+                return JsonResponse({"status": "Does not exist!"})
+            
+            body = request.body
+            # decode body
+            decode = body.decode()
+            data = json.loads(decode)
+            name = data.get('name')
+            completed = data.get('completed')
+            description = data.get('description')
+
+            task.name = data['name']
+            task.description = data['description']
+            task.completed = data['completed']
+            task.save()
+            return JsonResponse({"status": "Saccessfuly updated!"})
+
+class CompletedTasks(View):
+    def get(self, request: HttpRequest) -> JsonResponse:
+        tasks = Task.objects.filter(completed=True)
+        data = {
+            'tasks': []
+        }
+        for task in tasks:
+            data['tasks'].append(to_dict(task))
+
+        return JsonResponse(data)
+    
+class InCompletedTasks(View):
+    def get(self, request: HttpRequest) -> JsonResponse:
+        tasks = Task.objects.filter(completed=False)
+        data = {
+            'tasks': []
+        }
+        for task in tasks:
+            data['tasks'].append(to_dict(task))
+
+        return JsonResponse(data)
